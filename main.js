b94d039af90bea3f1a2a7dcc8b5156d4c03c1066
alexandrazvereva <alexandrazvereva7@gmail.com>
2023-05-12 13:27:42 -0600

    feat:add exersice solution

--- main.js
+++ main.js
@@ -1,31 +1,34 @@
-// У вас есть массив продуктов из корзины:
-
 let goods = ["skirt", "top", "shorts"];
-
-// 1 Добавьте в конец массива еще 2 продукта "sunglasses" и "hat"
-
-//ваш код
-
-console.log("Задача 1", goods)  // ["skirt", "top", "shorts", "sunglasses", "hat"]
+function addInBasket(item1, item2) {
+    goods.push(item1, item2)
+    console.log(goods);
+}
+addInBasket("sunglasses", "hat"); // ["skirt", "top", "shorts", "sunglasses", "hat"]
 
 //2 Добавьте в начало массива еще 3 продукта: "pants", "dress", "gloves"
+let list = ["skirt", "top", "shorts", "sunglasses", "hat"];
+function addInList(item1, item2, item3) {
+    list.unshift(item1, item2, item3)
+    console.log(list);
+}
 
-//ваш код
-
-console.log("Задача 2", goods)  // ["pants", "dress", "gloves", "skirt", "top", "shorts", "sunglasses", "hat"]
+addInList("pants", "dress", "gloves");  // ["pants", "dress", "gloves", "skirt", "top", "shorts", "sunglasses", "hat"]
 
 //3 Удалите из этого массива первый элемент
-
-//ваш код
-
-console.log("Задача 2", goods)  // ["dress", "gloves", "skirt", "top", "shorts", "sunglasses", "hat"]
+let things = ["pants", "dress", "gloves", "skirt", "top", "shorts", "sunglasses", "hat"];
+function deleteFromBasket() {
+    things.shift();
+    console.log(things);
+}
+deleteFromBasket(); // ["dress", "gloves", "skirt", "top", "shorts", "sunglasses", "hat"]
 
 // 4 Удалите последний элемент из этого массива
-
-//ваш код
-
-console.log("Задача 2", goods)  // ["pants", "dress", "gloves", "skirt", "top", "shorts", "sunglasses"]
-
+let cosas = ["pants", "dress", "gloves", "skirt", "top", "shorts", "sunglasses", "hat"];
+function removeFromBasket() {
+    cosas.pop();
+    console.log(cosas);
+}
+removeFromBasket(); // ["pants", "dress", "gloves", "skirt", "top", "shorts", "sunglasses"]
 
 //5 объедините эти три массива в один:
 
@@ -33,55 +36,62 @@ const cart1 = ["milk", "bread", "water"];
 const cart2 = ["sweets", "fanta", "cucumbers"];
 const cart3 = ["apples", "oranges", "juice"];
 
-//ваш код тут 
-
-console.log("Задача 5", {тут название вашего нового массива, в который объединятся все три}) //должен появиться в консоли массив, который содержит элементы этих трех массивов
-
-// Далее вы работаете с этим массивом:
-
-let users = ["admin", "seller", "customer"];
+function concatArrays() {
+    let newArray = cart1.concat(cart2, cart3); //console.log(cart1.concat(cart2, cart3))
+    console.log(newArray);
+}
+concatArrays();
 
 // 6 Выведите по очереди всех юзеров в консоль
-
-//ваш код тут
-
-// 7 Выведите в консоль фразы: {роль} is a user. 
+let users = ["admin", "seller", "customer"];
+function showUsers() {
+    users.forEach(element => console.log(element));
+}
+showUsers();
+// 7 Выведите в консоль фразы: {роль} is a user.
 // Всего в консоль должно вывестись 3 фразы по очереди :
 // admin is a user
 // seller is a user
 // customer is a user
+let user = ["admin", "seller", "customer"];
+function showUser() {
+    user.forEach((element,index) => console.log(element + ' is a user number ' + index ));
 
-//ваш код тут
-
-// 8 Выведите в консоль фразы: {роль } is user number {index}
-// Всего в консоль должно вывестись 3 фразы по очереди :
-// admin is user number 0
-// seller is user number 1
-// customer is user number 2
-
-// ваш код тут
+}
+showUser();
+//8 Выведите в консоль фразы: {роль } is user number {index}
+        // Всего в консоль должно вывестись 3 фразы по очереди :
+        // admin is user number 0
+        // seller is user number 1
+        // customer is user number 2
+    
 
 // 9 Создайте на основе массива users массив, в котором все названия ролей будут написаны большими буквами 
+let they = ["admin", "seller", "customer"];
+function showCaps() {
 
-//ваш код тут
+    they.forEach(element => console.log(element.toUpperCase()));
+};
 
-console.log("Задача 9", {тут название вашего массива}) // ["ADMIN", "SELLER", "CUSTOMER"]
-
+showCaps();// ["ADMIN", "SELLER", "CUSTOMER"]
 // 10 Создайте на основе массива user массив, в котором будут храниться длины слов-ролей
+let roles = ["admin", "seller", "customer"];
+function getLength() {
+    let newArray = roles.map(element => element.length);
+    console.log(newArray);
 
-// ваш код тут
-
-console.log("Задача 10", {тут название вашего массива}) // [5, 6, 8]
-
+}
+getLength();//[5, 6, 8]
 //11 Создайте на основе массива users массив, который будет хранить только те роли, чья длина - четное число (кратно двум)
+let people = ["admin", "seller", "customer"];
+function showresult() {
+    let newResult = roles.filter(element => element.length % 2 === 0);
+    console.log(newResult);
+};
+showresult();// ["seller", "customer"]
 
-//ваш код
-
-console.log("Задача 11", {тут название вашего массива}) // ["seller", "customer"]
-
-
-// Работа ведется с массивом products
-
+//12 Выведите в консоль информацию по каждому массиву в виде:
+// {productName} costs {price}. It's {kind}
 const products = [
     {
         productName: "apple",
@@ -109,34 +119,14 @@ const products = [
         price: "25"
     }
 ]
+function showArray(){
+products.forEach(element=>console.log(element.productName + ' costs ' + element.price + '. Its ' + element.kind))
+};
+showArray();
 
-//12 Выведите в консоль информацию по каждому массиву в виде:
-// {productName} costs {price}. It's {kind}
+function showFruits() {
+    let newFruits = products.filter(element => element.kind==="fruit");
+    console.log(newFruits);
 
-//ваш код тут
-
-// Должны получиться следуюшие строки:
-// apple costs 15. It's fruit
-// tomato costs 12. It's vegetable
-// sausage costs 50. It's meat
-// juice costs 20. It's drink
-// orange costs 25. It's fruit
-
-//13 Создайте на основе массива products массив, в котором будут храниться только фрукты. 
-
-//ваш код тут
-
-console.log("Задание 13", {тут название вашего массива}) 
-//должно вывестись в консоль:
-// [
-//     {
-//         productName: "apple",
-//         kind: "fruit",
-//         price: "15"
-//     },
-//     {
-//         productName: "orange",
-//         kind: "fruit",
-//         price: "25"
-//     }
-// ]
+}
+showFruits();
